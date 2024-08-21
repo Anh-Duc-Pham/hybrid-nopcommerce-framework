bf4f58a978d21468e72ba2b8ceb635067a42010e
Pham Anh Duc <phamanhduc111997@gmail.com>
2024-08-21 23:55:20 +0700

    Initial commit

--- interfaces/pageUIs/orangehrm/AddEmployeeUI.java
+++ interfaces/pageUIs/orangehrm/AddEmployeeUI.java
@@ -1,4 +1,7 @@
 package pageUIs.orangehrm;
 
 public class AddEmployeeUI {
+    public static final String DYNAMIC_NAME_TEXTBOX = "//input[@name='%s']";
+    public static final String EMPLOYEE_ID_TEXTBOX = "//label[text()='Employee Id']/parent::div/following-sibling::div/input";
+    public static final String SAVE_BTN = "xpath=//button[contains(string(),'Save')]";
 }
--- interfaces/pageUIs/orangehrm/EmployeeListUI.java
+++ interfaces/pageUIs/orangehrm/EmployeeListUI.java
@@ -1,4 +1,6 @@
 package pageUIs.orangehrm;
 
 public class EmployeeListUI {
+    public static final String ADD_EMPLOYEE_BTN = "xpath=//a[text()='Add Employee']";
+
 }
