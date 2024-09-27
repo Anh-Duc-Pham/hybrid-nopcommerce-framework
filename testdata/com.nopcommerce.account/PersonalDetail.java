f67ccc58c065abe1a4e59a2c38caeb0b7cbd1fc9
duc <phamanhduc111997@gmail.com>
2024-09-27 16:53:41 +0700

    company update orange

---
+++ testdata/com.nopcommerce.account/PersonalDetail.java
@@ -0,0 +1,14 @@
+package com.nopcommerce.account;
+
+public class PersonalDetail {
+    public static class PersonalInfo {
+        public static final String personName = "eden";
+        public static final String personAddress = "Hanoi";
+    }
+
+    public static class PersonalJob {
+        public static final String personTitle ="PM";
+        public static final String personRole ="Data";
+
+    }
+}
